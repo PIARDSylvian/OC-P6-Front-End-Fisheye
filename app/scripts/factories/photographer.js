b4b02d39156b943af18249612997b161751e5f00
PIARDSylvian <piard.sylvian@gmail.com>
2022-10-11 00:16:23 +0200

    extend function photographerFactory without class

--- app/scripts/factories/photographer.js
+++ app/scripts/factories/photographer.js
@@ -1,27 +1,43 @@
 function photographerFactory(data) {
+    const { name, portrait } = data;
+
+    const picture = `assets/photographers/${portrait}`;
+
+    function getUserCardDOM() {
+        const article = document.createElement( 'article' );
+        const img = document.createElement( 'img' );
+        img.setAttribute("src", picture)
+        const h2 = document.createElement( 'h2' );
+        h2.textContent = name;
+        article.appendChild(img);
+        article.appendChild(h2);
+        return (article);
+    }
+    return { name, picture, getUserCardDOM }
+}
+
+function photographerFactoryExtend(data) {
+    const proto = photographerFactory(data)
     const { name, portrait, city, country, tagline, price } = data;
     const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement('article');
-        const img = document.createElement('img');
+        const article = proto.getUserCardDOM()
+        const img = article.querySelector('img');
         img.setAttribute("src", picture);
         img.setAttribute("alt", name);
-        const h2 = document.createElement('h2');
-        h2.textContent = name;
         const location = document.createElement('p');
         location.textContent = `${city}, ${country}`;
         const tag = document.createElement('p');
         tag.textContent = tagline;
         const pricePerDay = document.createElement('p');
         pricePerDay.textContent = `${price}€/jour`;
-        article.appendChild(img);
-        article.appendChild(h2);
         article.appendChild(location);
         article.appendChild(tag);
         article.appendChild(pricePerDay);
         
         return (article);
     }
+
     return { name, picture, getUserCardDOM }
 }
--- app/scripts/pages/index.js
+++ app/scripts/pages/index.js
@@ -7,7 +7,7 @@
         const photographersSection = document.querySelector(".photographer_section");
 
         photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
+            const photographerModel = photographerFactoryExtend(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
         });
