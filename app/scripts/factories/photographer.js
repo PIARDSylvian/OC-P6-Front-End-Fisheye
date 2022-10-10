db8efb253681f04800a1f76ba678db66df8faaf0
PIARDSylvian <piard.sylvian@gmail.com>
2022-10-10 16:53:32 +0200

    add potographers data

--- app/scripts/factories/photographer.js
+++ app/scripts/factories/photographer.js
@@ -1,16 +1,26 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
-    const picture = `assets/photographers/${portrait}`;
+    const { name, portrait, city, country, tagline, price } = data;
+    const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement('article');
+        const img = document.createElement('img');
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", name);
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+        const location = document.createElement('p');
+        location.textContent = `${city}, ${country}`;
+        const tag = document.createElement('p');
+        tag.textContent = tagline;
+        const pricePerDay = document.createElement('p');
+        pricePerDay.textContent = `${price}€/jour`;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(tag);
+        article.appendChild(pricePerDay);
+        
         return (article);
     }
     return { name, picture, getUserCardDOM }
