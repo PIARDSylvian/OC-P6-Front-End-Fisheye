cd02bc11686a8368a214a35f423e78342a9176cb
PIARDSylvian <piard.sylvian@gmail.com>
2022-10-14 20:19:20 +0200

    get id

--- app/scripts/pages/photographer.js
+++ app/scripts/pages/photographer.js
@@ -1,1 +1,12 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+/**
+ * Get id in Url
+ * 
+ * @returns id
+ */
+function getUrlParmeterId (){
+    return window.location.hash.substr(1);
+}
+
+console.log("id : "+getUrlParmeterId());
